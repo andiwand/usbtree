1a9b38b8be0eda3e1c1250c9b60cb6a3836e53ad
andiwand <stefl.andreas@gmail.com>
2015-12-28 17:22:48 +0100

    finish usb

--- usb.py
+++ usb.py
@@ -1,7 +1,23 @@
-import re
 import os
+import re
+import util
 
-class usbpath:
+class UsbIdentifier:
+    def __init__(self, idVendor, idDevice):
+        self._vendor = idVendor if isinstance(idVendor, (int, long)) else int(idVendor, 16)
+        self._device = idDevice if isinstance(idDevice, (int, long)) else int(idDevice, 16)
+    def __str__(self):
+        return hex(self._vendor)[2:].zfill(4) + ":" + hex(self._device)[2:].zfill(4)
+    def __eq__(self, other):
+        return (isinstance(other, self.__class__)
+                and self._vendor == other._vendor
+                and self._device == other._device)
+    def __ne__(self, other):
+        return not self.__eq__(other)
+    def __hash__(self):
+        return self._vendor + 31 * self._device
+
+class UsbPath:
     def __init__(self, path=None, configuration=None, interface=None):
         if configuration == None: assert(interface == None)
         if interface == None: assert(configuration == None)
@@ -16,7 +32,7 @@ class usbpath:
         if len(self._path) == 0: return result
         result += str(self._path[0])
         if len(self._path) == 1: return result
-        result += "-" + ".".join(map(str, *self._path[1:]))
+        result += "-" + ".".join(map(str, self._path[1:]))
         if self._configuration != None: result += ":" + str(self._configuration) + "." + str(self._interface)
         return result
     def __eq__(self, other):
@@ -34,26 +50,16 @@ class usbpath:
                 and self._path[:len(other._path)] == other._path)
     def isParent(self, other):
         return other.isChild(self)
+    def getParent(self):
+        if len(self) == 0: return None
+        if self._configuration != None:
+            return UsbPath(self._path)
+        return UsbPath(self._path[:-1])
 
-class usbidentifier:
-    def __init__(self, idVendor, idDevice):
-        self._vendor = idVendor if isinstance(idVendor, (int, long)) else int(idVendor, 16)
-        self._device = idDevice if isinstance(idDevice, (int, long)) else int(idDevice, 16)
-    def __str__(self):
-        return hex(self._vendor)[2:].zfill(4) + ":" + hex(self._device)[2:].zfill(4)
-    def __eq__(self, other):
-        return (isinstance(other, self.__class__)
-                and self._vendor == other._vendor
-                and self._device == other._device)
-    def __ne__(self, other):
-        return not self.__eq__(other)
-
-class usbnode:
+class UsbNode:
     def __init__(self, path, identifier):
         self._path = path
         self._identifier = identifier
-        self._parent = None
-        self._children = set()
     def __str__(self):
         return str(self._path)
     def __eq__(self, other):
@@ -62,86 +68,75 @@ class usbnode:
     def __hash__(self):
         return hash(self._path)
     def isRoot(self):
-        return self._parent == None
+        pass
     def isLeave(self):
-        return len(self._children) == 0
-    def size(self):
-        return 1 + sum((child.size() for child in self._children))
-    def depth(self, i):
-        if self.isLeave(): return i
-        return max((child.depth(i + 1) for child in self._children))
-    def attach(self, other):
-        if other._parent != None: return False
-        if self == other: return False
-        if not other._path.isChild(self._path): return False
-        for child in self._children:
-            if child.attach(other): return True
-        children = set(self._children)
-        self._children.add(other)
-        other._parent = self
-        for child in children:
-            if child == other: continue
-            if child._path.isChild(other._path):
-                child.detach()
-                other.attach(child)
-        return True
-    def detach(self):
-        if self._parent == None: return False
-        self._parent._children.remove(self)
-        self._parent = None
-        return True
+        pass
 
-class usbtree:
+class UsbRegistry:
     def __init__(self):
-        self._root = usbnode(usbpath([]), None)
-        self._identifierMap = {}
-    def size(self):
-        return self._root.size() - 1
-    def depth(self):
-        return self._root.depth(0)
-    def add(self, node):
-        self._root.attach(node)
-        if node._identifier != None:
-            identifiers = self._identifierMap.get(node._identifier)
-            if identifiers == None: identifiers = self._identifierMap[node._identifier] = []
-            identifiers.add(node)
+        self._nodes = set()
+        self._byPath = {}
+        self._byIdentifier = {}
+    def addNode(self, node):
+        if node in self._nodes: return
+        self._nodes.add(node)
+        self._byPath[node._path] = node
+        if node._identifier not in self._byIdentifier:
+            self._byIdentifier[node._identifier] = set()
+        self._byIdentifier[node._identifier].add(node)
+    def removeNode(self, node):
+        if node not in self._nodes: return
+        self._nodes.remove(node)
+        del self._byPath[node._path]
+        self._byIdentifier[node._identifier].remove(node)
+        if not self._byIdentifier[node._identifier]:
+            del self._byIdentifier[node._identifier]
+    def getByPath(self, path):
+        return self._byPath.get(path)
+    def getByIdentifier(self, identifier):
+        return set(self._byIdentifier.get(identifier))
 
-class usbutil:
+class UsbUtil:
     @staticmethod
-    def usbtree():
-        result = usbtree()
+    def getRegistry():
+        result = UsbRegistry()
         path = "/sys/bus/usb/devices/"
-        for f in os.listdir(path):
-            subpath = os.path.join(path, f)
-            if not os.path.islink(subpath): continue
-            if not f.startswith("usb"): continue
-            usbutil._usbtree_helper(result, subpath)
+        for filename in os.listdir(path):
+            subpath = os.path.join(path, filename)
+            UsbUtil._getRegistryHelper(result, subpath)
         return result
     @staticmethod
-    def _usbtree_helper(tree, path):
+    def _getRegistryHelper(registry, path):
         filename = os.path.basename(path)
-        upath = usbutil._usbtree_path(filename)
-        for f in os.listdir(path):
-            subpath = os.path.join(path, f)
-            
+        usbpath = UsbUtil.toUsbPath(filename)
+        if usbpath == None: return
+        
+        vendorFile = os.path.join(path, "idVendor")
+        deviceFile = os.path.join(path, "idProduct")
+        usbidentifier = None
+        if os.path.isfile(vendorFile) and os.path.isfile(deviceFile):
+            usbidentifier = UsbIdentifier(util.readFile(vendorFile), util.readFile(deviceFile))
+        
+        node = UsbNode(usbpath, usbidentifier)
+        registry.addNode(node)
     @staticmethod
-    def _usbtree_path(f):
+    def toUsbPath(s):
         root_prefix = "usb"
-        if f.startswith(root_prefix):
-            num = int(f[len(root_prefix):])
-            return usbpath((num,))
-        file_regex = re.compile(r"(\d+)(?:-((?:(?:\d+\.)+)\d)(?:\:(\d+)\.(\d+))?)?")
-        m = file_regex.match(f)
+        if s.startswith(root_prefix):
+            num = int(s[len(root_prefix):])
+            return UsbPath((num,))
+        
+        file_regex = re.compile(r"(\d+)(?:-((?:(?:\d+\.)+)?\d+)(?:\:(\d+)\.(\d+))?)?")
+        m = file_regex.match(s)
         if m:
             path = []
             path.append(int(m.group(1)))
-            path.append(map(int, m.group(2).split(".")))
+            path.extend(map(int, m.group(2).split(".")))
             configuration = None
             interface = None
-            if m.groups > 2:
+            if m.group(3) != None:
                 configuration = int(m.group(3))
                 interface = int(m.group(4))
-            return usbpath(path, configuration, interface)
+            return UsbPath(path, configuration, interface)
+        
         return None
-
-#t = usbutil.usbtree()
---
+++ util.py
@@ -0,0 +1,4 @@
+
+def readFile(path):
+    with open(path, "r") as f:
+        return f.read()
